9fb82a6c40300f7a06e2b5884c156895b7ff8833
donngas <geunhyeongsam@gmail.com>
2025-11-22 21:13:17 +0900

    chore: complete basic test code (yet to be tested)

--- backend/core/inference.py
+++ backend/core/inference.py
@@ -1,46 +1,56 @@
 from google import genai
+from google.genai import types
 from os import getenv
 from pydantic import BaseModel
 from dotenv import load_dotenv
+from schema import GeminiResponse, AnalysisRequest
 import json
 
 load_dotenv()
 
 CONFIG_PATH = "./config.json"
 SYSPROMPT = """
-You are an expert academic advisor specializing in predicting student performance in university courses. 
+You are an expert academic advisor specializing in predicting is a student is fit for certain university courses. 
 
-You will be provided the following 
+You will be provided the student's following information:
+- The student's prior taken classes
+- Evaluation method preference, projects/assignments(1) vs. written tests(5) 
+- Interested areas as keywords
+- Emphasis on cooperative projects/assignments (Prefer team project: 5 - Hate team project: 1)
+- Teaching/Attendence methods: Online vs. offline, Electronic vs. verbal(any item mentioned is what user prefers. If both are chosen, user is fine with both.)
+
+On each axis, you are to return fitness score between 1 to 5 (integers), as well as approx. 2 sentences long explanation for each axis, in the given json format.
+
+Candidate courses and the student's individual information will be provided below.
+
+Write all text content in Korean.
 """
 
 client = genai.Client()
-
-class GeminiResponse(BaseModel):
-    risk: str
-    risk_text: str
-    grade_range: str
-
-class UserData(BaseModel):
-    pass
 
 # Util function to open and load json file
 def load_json(path: str):
     with open(path, "r", encoding="utf8") as f:
         return json.load(f)
     
-def construct_user_prompt():
-    pass
+def call_gemini(user_prompt: AnalysisRequest):
 
-def call_gemini(prompt: str):
+    # Define config
+    config = types.GenerateContentConfig(
+        system_instruction=SYSPROMPT,
+        response_mime_type="application/json",
+        response_schema=GeminiResponse
+    )
 
+    # Get user prompt
+    user_prompt = AnalysisRequest
+
+    # Make request
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=[SYSPROMPT, prompt],
+        contents=user_prompt,
         # Structured Response config
-        config={
-            "response_mime_type": "application/json",
-            "response_schema": GeminiResponse,
-        })
+        config=config)
 
     return response.text
 
