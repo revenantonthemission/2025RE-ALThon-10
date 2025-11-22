2143c432366de7a56eb84fbf959b3061ed4f7856
donngas <geunhyeongsam@gmail.com>
2025-11-22 19:14:52 +0900

    feat: basic test code

--- backend/core/inference.py
+++ backend/core/inference.py
@@ -1,21 +1,31 @@
 from google import genai
 from os import getenv
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import json
 
-# SYSPROMPT = 
+load_dotenv()
+
+CONFIG_PATH = "./config.json"
+SYSPROMPT = ""
 
 client = genai.Client()
 
-class GeminiResponse:
+class GeminiResponse(BaseModel):
     risk: str
     risk_text: str
     grade_range: str
+
+# Util function to open and load json file
+def load_json(path: str):
+    with open(path, "r", encoding="utf8") as f:
+        return json.load(f)
 
 def call_gemini(prompt: str):
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=prompt,
+        contents=[SYSPROMPT, prompt],
         # Structured Response config
         config={
             "response_mime_type": "application/json",
@@ -23,3 +33,12 @@ def call_gemini(prompt: str):
         })
 
     return response.text
+
+if __name__=="__main__":
+    
+    config = load_json(CONFIG_PATH)
+    SYSPROMPT = config.get("sys_prompt")
+
+    prompt = ""
+
+    print(call_gemini(prompt))
